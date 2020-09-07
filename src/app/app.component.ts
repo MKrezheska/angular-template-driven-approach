d2a4e3fed15c24305ab20d552fcc9f6f5f140868
Magdalena Krezheska <Magdalena.Krezheska@netcetera.com>
2020-09-07 15:05:40 +0200

    ViewChild - get access to the form without passing to the funstion onSubmit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('f', { static: false }) signupForm: NgForm;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
   onSubmit(form: NgForm) {
-    console.log(form);
+    console.log(this.signupForm);
   }
 }
